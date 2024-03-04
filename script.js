d98e5f6e5da3486469318d49218609365bac9162
Sidharth <148176670+sidharthchawla484@users.noreply.github.com>
2024-03-04 09:51:28 +0530

    Add files via upload
    Personal portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+/**     typing animation*/
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","Full-Stack Developer","Frontend Developer","Backend Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
